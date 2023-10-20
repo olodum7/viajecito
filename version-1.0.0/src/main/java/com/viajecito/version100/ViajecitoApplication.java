0447f9c3d8ac59aad83a3cdaa068831f0da5b018
rodezf <franrodez33@gmail.com>
2023-10-20 03:42:48 -0300

    feat: springboot set up

---
+++ version-1.0.0/src/main/java/com/viajecito/version100/ViajecitoApplication.java
@@ -0,0 +1,13 @@
+package com.viajecito.version100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajecitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajecitoApplication.class, args);
+	}
+
+}
