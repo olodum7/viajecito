658dac3ef4180730cf68b7dfec915ef2a50772f4
CamilaFortunato <camilaafortunatoo@gmail.com>
2023-10-25 16:03:52 -0300

    Css de detail

--- Front/src/routes/Detail.jsx
+++ Front/src/routes/Detail.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react"
 import {useParams} from 'react-router-dom'
+import '../style/Detail.css'
 
 export function Detail () {
     const { id } = useParams();
@@ -18,25 +19,42 @@ export function Detail () {
 
 
     return (
-        <div>
+        <div className="caja">
             <div className='S1-Imagenes'>
-                <h1>Imagenes</h1>
+                <h3 className="Imagen">img</h3>
+                <div className="Imagen">img</div>
             </div>
 
             <div className='S2-Datos'>
                 <div className='A1-Info'>
-                    <h1>iNFO</h1>
-                    <p>la categoria es: {tour.categoria}</p>
+                    <h1>Titulo</h1>
+                    <hr />
+                    <h3>El paquete incluye:</h3>
+                    <ul>
+                        <li>Desayuno</li>
+                        <li>Pasaje</li>
+                    </ul>
+                    <hr />
+                    <h3>Opcionales</h3>
+                    
+                    {/* <p>la categoria es: {tour.categoria}</p>*/}
                 </div>
+
                 <div className='A2-Cantidad'>
-                    <h1>CANTIDAD</h1>
+                    <h3>Cantidad</h3>
+                    <h3>Fechas</h3>
+                    <h3>USD</h3>
                 </div>
             </div>
 
+            <hr />
+
             <div className='S3-Itinerario'>
-                <h1>ITINERARIO</h1>
+                <h2>Itinerario</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolor placeat vero eos, nostrum excepturi cumque quod velit hic dolorem, nesciunt accusamus, architecto perferendis a quibusdam esse nisi magnam mollitia.</p>
+                    
             </div>
-            
+
             {/*tour.name - tour.img....*/}
             
         </div>
