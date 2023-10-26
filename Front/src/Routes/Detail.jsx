607254b96efe2674ce8ffc7f723bde4555b6a03b
Karen1308 <karengarcia1308@gmail.com>
2023-10-26 01:12:47 -0300

    Merge con rama 06
    No hay problemas
    Pendiente conexión con API

--- Front/src/Routes/Detail.jsx
+++ Front/src/Routes/Detail.jsx
@@ -1,46 +1,83 @@
-import React, { useEffect, useState } from "react"
-import '../style/Detail.css'
-import Imagen from "../Components/Imagen";
+import React, {useEffect, useState} from "react"
+import {useParams} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-export function Detail({ id }) {
-    const [result, setResult] = useState("");
+import {ImageGallery} from './ImageGallery';
 
-    useEffect(() => {
-        fetch(`https://localhost:8089/tour/${id}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setResult(data);
-            })
-            .catch((error) => {
-                console.error("Error al obtener los tours: \n", error)
-            })
-    },[]);
+import '../style/Detail.css';
+
+
+//las importé así porque no sabía cómo usar la API:
+import image1 from './imagenesPrueba/1.jpg';
+import image2 from './imagenesPrueba/2.jpg';
+import image3 from './imagenesPrueba/3.jpg';
+import image4 from './imagenesPrueba/4.jpg';
+import image5 from './imagenesPrueba/5.jpg';
+//la carpeta de estas imágenes provisorias está dentro de "routes"
+
+
+export function Detail () {
+    const { id } = useParams();
+    const [tour, setTour] = useState({}); 
+
+    
+    const getTour = async () => {
+    const response = await fetch(`https://localhost:8089/tour/${id}`);
+    const data = await response.json();
+    setTour(data);
+  }
+
+  useEffect(() => {
+    getTour()
+  })
+
 
     return (
-        <div className="caja">
-            <div className='S1-Imagenes'>
-                {/* {result.forEach(imagen => {
-                    <Imagen nombre={imagen.nombre} />
-                });
-                } */}
-            </div>
+        <div>
+            <div className='S1-galeria-imagenes'>
+                
+                <div className="parte-superior-galeria">
+                    <div className="image-container">
+                        <img src={image1} alt="Imagen principal" />
+                    </div>
+
+                    <div className="image-grid">
+                        <img src={image2} alt="Imagen 2" />
+                        <img src={image3} alt="Imagen 3" />
+                        <img src={image4} alt="Imagen 4" />
+                        <img src={image5} alt="Imagen 5" />
+                    </div>
+                </div>
+                
+
+                <div className="parte-inferior-galeria">
+                    <div className="ver-mas-button">
+                        <Link to="/image-gallery">
+                            Ver más
+                        </Link>
+                    </div>
+                </div>
+
+            </div>{/* endS1-galeria-imagenes */}
+
+
 
             <div className='S2-Datos'>
                 <div className='A1-Info'>
-                    <h1>{result.id}</h1>
-                    <p>la categoria es: {result.categoria}</p>
+                    <h1>iNFO</h1>
+                    <p>la categoria es: {tour.categoria}</p>
                 </div>
                 <div className='A2-Cantidad'>
-                    <h3>{result.descripcion}</h3>
-                    <h3>Fechas</h3>
-                    <h3>USD</h3>
+                    <h1>CANTIDAD</h1>
                 </div>
             </div>
-            <hr />
+
+
+
             <div className='S3-Itinerario'>
-                <h2>Itinerario</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolor placeat vero eos, nostrum excepturi cumque quod velit hic dolorem, nesciunt accusamus, architecto perferendis a quibusdam esse nisi magnam mollitia.</p>
+                <h1>ITINERARIO</h1>
             </div>
-        </div>
+        </div>                        
     )
+
 }
