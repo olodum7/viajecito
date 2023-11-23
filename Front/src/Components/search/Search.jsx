1eca8b29cfe1f76441baafa2a13c3c839933346d
Emanuel Espinel <olodum7@gmail.com>
2023-11-22 22:28:04 -0300

    Historia de usuario #22

--- Front/src/Components/search/Search.jsx
+++ Front/src/Components/search/Search.jsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import SearchButton from '../buttons/SearchButton'
+import SearchButton from "../buttons/SearchButton";
 
-export function Search() {
-  const [country, setCountry] = useState("");
-  const [people, setPeople] = useState("");
+export function Search({ onSearchChange, onSearchSubmit, search }) {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
 
-  const handleCountryChange = (event) => {
-    setCountry(event.target.value);
-  };
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-  const handlePeopleChange = (event) => {
-    setPeople(event.target.value);
-  };
+  const keywords = ["Tailandia", "Camboya", "Vietnam", "Auroras en el norte", "Artico", "Japón", "Tradición", "Andino", "Bicicleta", "Gourmet", "Lima", "Riviera", "Maya", "Cenotes", "Ruinas", "Playas paradisíacas", "Patagonia Argentina", "Bariloche", "Chivito uruguayo", "Rio de Janeiro", "Uyuni", "Salar de Uyuni", "Bolivia"];
 
   const handleStartDateChange = (date) => {
     setStartDate(date);
@@ -25,9 +20,32 @@ export function Search() {
     setEndDate(date);
   };
 
+  const handleSearchChange = (event) => {
+    const value = event.target.value;
+    onSearchChange(value);
+
+    if (value.length > 2) {
+      // Empieza a sugerir después de 2 caracteres
+      const filteredSuggestions = keywords.filter((keyword) =>
+        keyword.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+      setShowSuggestions(true);
+    } else {
+      setShowSuggestions(false);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    onSearchChange(suggestion);
+    setShowSuggestions(false);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Aquí puedes realizar alguna acción con los valores seleccionados
+    if (search.trim() !== "") {
+      onSearchSubmit();
+    }
   };
 
   return (
@@ -35,64 +53,32 @@ export function Search() {
       <div className="container">
         <form onSubmit={handleSubmit}>
           <div className="row justify-content-center align-items-center d-flex">
-            <div className="col-lg-3">
-              <div className="form-group">
-                <label htmlFor="country">Lugar</label>
-                <select
-                  id="country"
-                  name="country"
+            <div className="col-12 col-lg-7">
+              <div className="form-group search-container">
+                <label htmlFor="search-input">Busca tu próxima aventura</label>
+                <input
+                  id="search-input"
+                  type="text"
                   className="form-control"
-                  value={country}
-                  onChange={handleCountryChange}
-                >
-                  <option value="" disabled className="inactive-option">
-                    ¿A dónde te gustaría ir?
-                  </option>
-                  <option value="usa" className="selected-option">
-                    Estados Unidos
-                  </option>
-                  <option value="canada" className="selected-option">
-                    Canadá
-                  </option>
-                  <option value="mexico" className="selected-option">
-                    México
-                  </option>
-                  <option value="spain" className="selected-option">
-                    España
-                  </option>
-                  <option value="france" className="selected-option">
-                    Francia
-                  </option>
-                </select>
+                  placeholder="Escribe el nombre del destino que deseas explorar..."
+                  value={search}
+                  onChange={handleSearchChange}
+                />
+                {showSuggestions && (
+                  <ul className="suggestions-list">
+                    {suggestions.map((suggestion, index) => (
+                      <li
+                        key={index}
+                        onClick={() => handleSuggestionClick(suggestion)}
+                      >
+                        {suggestion}
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
             </div>
-            <div className="col-lg-4">
-              <div className="form-group">
-                <label htmlFor="people">Personas</label>
-                <select
-                  id="people"
-                  name="people"
-                  className="form-control"
-                  value={people}
-                  onChange={handlePeopleChange}
-                >
-                  <option value="" disabled>
-                    ¿Cuántas personas viajarán contigo?
-                  </option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-            </div>
-            <div className="col-lg-4">
+            <div className="col-12 col-lg-4">
               <div className="form-group">
                 <label htmlFor="date-range">Fechas</label>
                 <div className="input-daterange input-group">
