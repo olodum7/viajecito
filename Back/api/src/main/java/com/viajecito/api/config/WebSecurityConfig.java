2dea2aaf90a6de8b0a5efe356d867d3df87c41fd
Emanuel Espinel <olodum7@gmail.com>
2023-12-06 09:58:55 -0300

    Probando habilitar cors en el security

--- Back/api/src/main/java/com/viajecito/api/config/WebSecurityConfig.java
+++ Back/api/src/main/java/com/viajecito/api/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.viajecito.api.config;
 
-import com.viajecito.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,8 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -23,40 +24,60 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-                http.headers().frameOptions().sameOrigin();
                 http
+                        // Habilitar CORS
+                        .cors().and()
+                        // Deshabilitar CSRF
                         .csrf().disable()
+                        // Configurar las solicitudes autorizadas
                         .authorizeRequests()
-                                .antMatchers("/db.ctd.academy:3306/**").permitAll()
-                                .antMatchers("/assets/**").permitAll()
-                                .antMatchers("/public/**").permitAll()
-                                .antMatchers("/usuario").permitAll()
-                                .antMatchers( "/register").authenticated()
-                                .and()
+                        .antMatchers("/db.ctd.academy:3306/**").permitAll()
+                        .antMatchers("/assets/**").permitAll()
+                        .antMatchers("/public/**").permitAll()
+                        .antMatchers("/usuario").permitAll()
+                        .antMatchers("/register").authenticated()
+                        .and()
+                        // Configuración de formulario de login
                         .formLogin()
-                                .permitAll()
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/index",true)
-                                .failureUrl("/login?error=true")
-                                .and()
+                        .permitAll()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/index", true)
+                        .failureUrl("/login?error=true")
+                        .and()
+                        // Configuración de logout
                         .logout()
-                                .logoutSuccessUrl("/login?logout=true")
-                                .permitAll()
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
-                                .and()
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll()
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .and()
+                        // Manejo de excepciones
                         .exceptionHandling().accessDeniedPage("/error/403");
         }
 
         @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
                 auth.authenticationProvider(daoAuthenticationProvider());
         }
+
         @Bean
         public DaoAuthenticationProvider daoAuthenticationProvider() {
                 DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
                 provider.setPasswordEncoder(new BCryptPasswordEncoder());
                 provider.setUserDetailsService(userService);
                 return provider;
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("http://1023c01-grupo1-s3-front.s3-website-us-east-1.amazonaws.com"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin"));
+                configuration.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
         }
 }
